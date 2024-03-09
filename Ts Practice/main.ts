5f109c858de258083dee0fc269ff7d2fcace7c94
kashifalismit <kashifalismitofficial@gmail.com>
2024-03-09 18:27:42 +0500

    ts pratice

---
+++ Ts Practice/main.ts
@@ -0,0 +1,54 @@
+//  Uniary operator
+//  Bmi
+//  121
+
+// let layer = 5;
+// let apple = 5;
+// let power = layer ** 3;
+// console.log(power);
+
+
+
+
+
+
+// let num1 = 5;
+// let num2 = 28;
+// let num3 = 13;       
+//                     5+1=6  + 28+1=29 + 8 + 13+1=14 ==>57 
+// let totalincrement= ++num1 + ++num2  + 8 + ++num3;
+// console.log(totalincrement);
+
+
+
+
+
+
+// let num4 = 15;
+// let num5 = 68;
+// let num6 = 88;       
+
+// let totalincrement = ++num4 + --num5 + ++num5 + --num4   + --num6;
+// console.log(totalincrement);
+
+
+
+
+
+
+// let a=11; 
+// let b=22; 
+//         11 + 22 + 11 +  22  + 13 + 24
+// let c = a  + b  + a++ + b++ + ++a + ++b;
+// console.log(c);
+
+
+
+
+
+let a: number = 5;
+let b: number = 2;
+let c: number;
+c = ++a + a++ + --b + b-- + a + b++ + b;
+6 + 6 + 1 + 1 + 7 + 1 + 1
+console.log(c);
