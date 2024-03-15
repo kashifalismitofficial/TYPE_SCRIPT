a4ef27b4ff04f10ba4c76b5d1e33725f11340e38
kashifalismitofficial <kashifalismitofficail>
2024-03-16 00:24:46 +0500

    ipm git commit -mipm git commit -mipm

--- Ts Practice/main.js
+++ Ts Practice/main.js
@@ -8,7 +8,7 @@
 // let num1 = 5;
 // let num2 = 28;
 // let num3 = 13;       
-//  5+1=6  + 28+1=29 + 8 + 13+1=14 ==>57 
+//                     5+1=6  + 28+1=29 + 8 + 13+1=14 ==>57 
 // let totalincrement= ++num1 + ++num2  + 8 + ++num3;
 // console.log(totalincrement);
 // let num4 = 15;
@@ -21,9 +21,29 @@
 //         11 + 22 + 11 +  22  + 13 + 24
 // let c = a  + b  + a++ + b++ + ++a + ++b;
 // console.log(c);
-var a = 5;
-var b = 2;
-var c;
-c = ++a + a++ + --b + b-- + a + b++ + b;
-6 + 6 + 1 + 1 + 7 + 1 + 1;
-console.log(c);
+// let a: number = 5;
+// let b: number = 2;
+// let c: number;
+// c = ++a + a++ + --b + b-- + a + b++ + b;
+// 6 + 6 + 1 + 1 + 7 + 1 + 1
+// console.log(c);
+var weghtinkg = 70;
+var hieghtinmeters = 1.75;
+var bmi = weghtinkg / (hieghtinmeters * hieghtinmeters);
+console.log("here is your bmi==>", bmi);
+var num1 = 5;
+var num2 = 55;
+var mutli = num1 * num2;
+console.log(mutli);
+var num3 = 5;
+var num4 = 55;
+var divi = num3 / num4;
+console.log(divi);
+var num5 = 5;
+var num6 = 55;
+var sub = num5 - num6;
+console.log(sub);
+var num7 = 5;
+var num8 = 55;
+var add = num7 + num8;
+console.log(add);
--- Ts Practice/main.ts
+++ Ts Practice/main.ts
@@ -46,9 +46,41 @@
 
 
 
-let a: number = 5;
-let b: number = 2;
-let c: number;
-c = ++a + a++ + --b + b-- + a + b++ + b;
-6 + 6 + 1 + 1 + 7 + 1 + 1
-console.log(c);
+// let a: number = 5;
+// let b: number = 2;
+// let c: number;
+// c = ++a + a++ + --b + b-- + a + b++ + b;
+// 6 + 6 + 1 + 1 + 7 + 1 + 1
+// console.log(c);
+
+
+let weghtinkg = 70;
+let hieghtinmeters = 1.75;
+let bmi = weghtinkg / (hieghtinmeters * hieghtinmeters);
+console.log("here is your bmi==>", bmi);
+
+
+let num1 = 5;
+let num2 = 55;
+let mutli = num1 * num2
+console.log(mutli);
+
+
+let num3 = 5;
+let num4 = 55;
+let divi = num3 / num4
+console.log(divi);
+
+
+let num5 = 5;
+let num6 = 55;
+let sub = num5 - num6
+console.log(sub);
+
+
+let num7 = 5;
+let num8 = 55;
+let add = num7 + num8
+console.log(add);
+
+
