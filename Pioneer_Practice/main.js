92fd6bcdca4e21b07d327cabbd610a0ddab74524
kashifalismitofficial <kashifalismitofficail>
2024-03-19 00:30:40 +0500

    practice

---
+++ Pioneer_Practice/main.js
@@ -0,0 +1,29 @@
+// var redecalre reasign 
+// let reassign 
+// cont declare
+// var num = 5;
+// var num = 50;
+// num = 5;
+// let num1 = 50 ;
+//     num1 = 50 ;
+// const num2 = 55;
+// const num2 = 55;
+//       num2 = 55;
+// Array
+//  0         1        2       3        4
+// let fruits = ["Apple", "Banana", "Orange", "Grapes", "Mango"];
+// console.log(fruits.length);
+// console.log(typeof (fruits));
+// tsc main.ts 
+// node main.js
+// tsc main.ts && node main.js
+// let num = 5;
+// console.log(typeof(num));
+// let fruits = ["Apple", "Banana", "Orange", "Grapes", "Mango"];
+var age = 15;
+if (age < 14) {
+    console.log("he is teenager");
+}
+else {
+    console.log("Because he is Adult");
+}
