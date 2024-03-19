c7096f07cdeb79e6c8fe76f146a1c2417a98c81c
kashifalismitofficial <kashifalismitofficail>
2024-03-19 14:48:08 +0500

    simple calculator

---
+++ simple_calculator/index.ts
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+{message: "Enter First Number", type:"number", name:"firstNumber"},
+{message:"Enter Second Number",type:"number",name:"SecondNumber"},
+{message:"Select one of the operators to perform operation",type:"list",name:"operator", choices:["Addition","Subtraction","Multiplication","Division"]},
+]);
+// Conditional Statement
+if (answer.operator === "Addition"){
+    console.log(answer.firstNumber + answer.SecondNumber);  
+}else if(answer.operator ==="Subtraction"){
+    console.log(answer.firstNumber - answer.SecondNumber);
+}else if(answer.operator ==="Multiplication"){
+    console.log(answer.firstNumber * answer.SecondNumber);
+}else if(answer.operator ==="Division"){
+    console.log(answer.firstNumber / answer.SecondNumber);
+}else{
+    console.log(`Please Select Valid Operator`);
+}
