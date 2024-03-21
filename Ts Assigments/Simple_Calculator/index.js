108ed9fdb76e1c8280eeb2c97d2fc69b43b67de7
kashifalismitofficial <kashifalismitofficail>
2024-03-21 12:57:54 +0500

    publish my pkage+

--- Ts Assigments/Simple_Calculator/index.js
+++ Ts Assigments/Simple_Calculator/index.js
@@ -1,5 +1,8 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
-const answer = await inquirer.prompt([{ message: "Enter First Number", type: "number", name: "firstNumber" }, { message: "Enter Second Number", type: "number", name: "SecondNumber" },
+const answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "firstNumber" },
+    { message: "Enter Second Number", type: "number", name: "SecondNumber" },
     { message: "Select one of the operators to perform operation", type: "list", name: "operator", choices: ["Addition", "Subtraction", "Multiplication", "Division"] },
 ]);
 // Conditional Statement
